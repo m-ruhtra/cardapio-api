93c308556937091ad41285f2684a5b7505947e2a
m-ruhtra <arthur.mig.rodrigues@gmail.com>
2024-03-21 20:13:53 -0300

    feat: configuração de tratativa de erros

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
+import "express-async-errors";
 import cors from "cors";
 import express, { json } from "express";
 import helmet from "helmet";
+import { HandleErrors } from "./middleware/handleErrors.middleware";
 
 export const app = express();
 
 app.use(cors());
+
 app.use(helmet());
+
 app.use(json());
+
+app.use(HandleErrors.execute);
--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { app } from "./app";
 const port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`API sucessfully started at por ${port}`);
+    console.log(`API sucessfully started at por ${port}`);
 });
